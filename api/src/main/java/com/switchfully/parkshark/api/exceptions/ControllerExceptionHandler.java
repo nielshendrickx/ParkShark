aaeee31386a4b0cf40d5082d2897afe36290477d
nielsh <Niemand@01>
2020-04-20 10:08:37 +0200

    Skeleton

---
+++ api/src/main/java/com/switchfully/parkshark/api/exceptions/ControllerExceptionHandler.java
@@ -0,0 +1,11 @@
+package com.switchfully.parkshark.api.exceptions;
+
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+
+@ControllerAdvice
+public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
+
+}
