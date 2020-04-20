d6a58522d2c4be60eca533fb6902ff4f2012ee18
MatiasVL <matias.vanlangenhove@gmail.com>
2020-04-20 14:34:08 +0300

    createdMemberRepo

---
+++ domain/src/main/java/com/switchfully/parkshark/domain/user/member/MemberRepository.java
@@ -0,0 +1,11 @@
+package com.switchfully.parkshark.domain.user.member;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MemberRepository extends CrudRepository<Member, Long> {
+    List<Member> findAll();
+
+    Member save(Member member);
+}
