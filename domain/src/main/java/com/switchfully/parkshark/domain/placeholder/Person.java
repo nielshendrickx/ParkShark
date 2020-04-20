70ef9ebab01f5928d9cb326e094cfc2a130eafa7
Sven Verboven <sven_verboven@hotmail.com>
2020-04-20 12:59:02 +0200

    added Person.java added database configuration

---
+++ domain/src/main/java/com/switchfully/parkshark/domain/placeholder/Person.java
@@ -0,0 +1,22 @@
+package com.switchfully.parkshark.domain.placeholder;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "person")
+public class Person {
+
+    @Id
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "firstName")
+    private String firstName;
+
+    public Person() {
+    }
+}
