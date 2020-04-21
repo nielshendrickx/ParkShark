5f1a4049a97b30689e7e5abde298530098ea7733
Sven Verboven <sven_verboven@hotmail.com>
2020-04-21 15:26:11 +0200

    added logging

--- api/src/main/java/com/switchfully/parkshark/api/endpoints/DivisionController.java
+++ api/src/main/java/com/switchfully/parkshark/api/endpoints/DivisionController.java
@@ -3,6 +3,8 @@ package com.switchfully.parkshark.api.endpoints;
 import com.switchfully.parkshark.service.division.CreateDivisionDTO;
 import com.switchfully.parkshark.service.division.DivisionDTO;
 import com.switchfully.parkshark.service.services.DivisionService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +18,7 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 @RequestMapping(path = "/divisions")
 public class DivisionController {
 
+    private final Logger loggerDivision = LoggerFactory.getLogger(DivisionController.class);
     private final DivisionService divisionService;
 
     @Autowired
@@ -26,18 +29,21 @@ public class DivisionController {
     @PostMapping(consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.CREATED)
     public DivisionDTO createDivision(@Valid @RequestBody CreateDivisionDTO createDivisionDTO) {
+        loggerDivision.info("Creating a new division");
         return divisionService.saveDivision(createDivisionDTO);
     }
 
     @GetMapping(produces = APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.OK)
     public List<DivisionDTO> getAllDivisions() {
+        loggerDivision.info("Returning all divisions");
         return divisionService.getAllDivisions();
     }
 
     @PostMapping(path = "/{primaryKeyId}/{parentDivisionKey}")
     @ResponseStatus(HttpStatus.CREATED)
     public void createSubDivision(@PathVariable int primaryKeyId,@PathVariable int parentDivisionKey) {
+        loggerDivision.info("Assign subdivision");
         divisionService.createSubDivision(parentDivisionKey,primaryKeyId);
     }
 
